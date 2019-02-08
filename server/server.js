40028e8e143052d5e833eea41712f67511432bcc
Anmol Middha <anmolmiddha@localhost.localdomain>
2019-02-08 22:45:42 +0530

    new commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,12 +11,29 @@ app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
 
-const db = require("./config/keys").mongoURI;
+const db = require("../config/key").mongoURI;
 mongoose.connect(db, {useNewUrlParser: true});
 
-const expense = require('./routes/expense.js');
+const expenseRoute = require('./routes/expense.js');
 
-app.use('/', expense);
+app.use('/', expenseRoute);
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+            error
+        }
+    });
+}); 
 
 module.exports = app;
